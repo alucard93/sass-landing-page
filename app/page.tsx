55129a2765c88b47495caa3353939ba12403e0db
alucard93 <dev.mvinicius@gmail.com>
2026-03-26 18:46:26 -0300

    chore: update dependencies and add new packages for improved styling and functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,102 +1,103 @@
-  import Image from 'next/image'
-  import logo from './assets/logo.svg'
-  import woman from './assets/woman.svg'
+import Image from 'next/image'
+import logo from './assets/logo.svg'
+import woman from './assets/woman.svg'
+import { Button } from '@/components/ui/button'
 
-  export default function Home() {
-    return (
-      <main>
-        <section className="container mx-auto">
-          <nav className="flex justify-between items-center py-4">
-            <div>
-              <Image
-                src={logo}
-                alt="Logo"
-                width={129}
-                height={40}
-                style={{ width: 'auto', height: 'auto' }}
-              />
-            </div>
-            <ul className="flex gap-2">
-              <li>
-                <button>Funcionamento</button>
-              </li>
-              <li>
-                <button>Preço</button>
-              </li>
-              <li>
-                <button>Login</button>
-              </li>
-            </ul>
-          </nav>
-          <h1>Simplifique Seus Estudos</h1>
-          <p>
-            Deixe que nós fazermos a curadoria para você. Assine nossa plataforma
-            e receba todos os meses um ebook novo de programação.
-          </p>
-
-          <form action="">
-            <input type="text" placeholder="Coloque seu email" />
-            <button>Assine Agora</button>
-            <p>Comece sua assinatura agora mesmo. Cancele quando quiser.</p>
-          </form>
-        </section>
-        {/* 2ª SEÇÃO */}
-        <section className="bg-white">
-          <h2>Como funciona?</h2>
-          <Image
-            src={woman}
-            alt="Woman"
-            style={{ width: 'auto', height: 'auto' }}
-          />
-          <ul>
-            <li>Acesso a 1 ebook por mês</li>
-            <li>Curadoria especial</li>
-            <li>Cancele quando quiser</li>
-          </ul>
-        </section>
-
-        <section>
-          <h2>Preço simples e transparente</h2>
-          <p>
-            Pra que inúmeros planos quando nós sabemos exatamente o que é melhor
-            para você? Assine o nosso plano mensal Pro Premium VIP e garanta
-            mensalmente um ebook novo de programação. E por menos de um café por
-            dia.{' '}
-          </p>
+export default function Home() {
+  return (
+    <main>
+      <section className="container mx-auto">
+        <nav className="flex justify-between items-center py-4">
           <div>
-            <h3>Plano Pro Premiun VIP</h3>
-          </div>
-          <p>Tudo que você precisa para seus estudos</p>
-          <p>R$29/mês</p>
-
-          <ul>
-            <li>1 ebook por mês</li>
-            <li>Curadoria especial</li>
-            <li>Acesso ilimitado</li>
-            <li>Cancele a qualquer momento</li>
-          </ul>
-          <button>Assina Agora</button>
-        </section>
-
-        <section className="bg-white">
-          <h2>Pronto Para Mudar Sua Vida?</h2>
-          <p>
-            Faça como milhares de outras pessoas. Assine nosso produto e tenha
-            garantido seus estudos{' '}
-          </p>
-          <button>Assine Agora</button>
-          <p>Comece sua assinatura agora mesmo. Cancele quando quiser. </p>
-          <footer>
             <Image
               src={logo}
               alt="Logo"
-              width={16}
-              height={16}
+              width={129}
+              height={40}
               style={{ width: 'auto', height: 'auto' }}
             />
-            <p>© 2024 LivroSaaS. Todos os direitos reservados.</p>
-          </footer>
-        </section>
-      </main>
-    )
-  }
+          </div>
+          <ul className="flex gap-1">
+            <li>
+              <Button variant={'link'}>Funcionamento</Button>
+            </li>
+            <li>
+              <Button variant={'link'}>Preço</Button>
+            </li>
+            <li>
+              <Button variant={'bg-white'}>Login</Button>
+            </li>
+          </ul>
+        </nav>
+        <h1>Simplifique Seus Estudos</h1>
+        <p>
+          Deixe que nós fazermos a curadoria para você. Assine nossa plataforma
+          e receba todos os meses um ebook novo de programação.
+        </p>
+
+        <form action="">
+          <input type="text" placeholder="Coloque seu email" />
+          <button>Assine Agora</button>
+          <p>Comece sua assinatura agora mesmo. Cancele quando quiser.</p>
+        </form>
+      </section>
+      {/* 2ª SEÇÃO */}
+      <section className="bg-white">
+        <h2>Como funciona?</h2>
+        <Image
+          src={woman}
+          alt="Woman"
+          style={{ width: 'auto', height: 'auto' }}
+        />
+        <ul>
+          <li>Acesso a 1 ebook por mês</li>
+          <li>Curadoria especial</li>
+          <li>Cancele quando quiser</li>
+        </ul>
+      </section>
+
+      <section>
+        <h2>Preço simples e transparente</h2>
+        <p>
+          Pra que inúmeros planos quando nós sabemos exatamente o que é melhor
+          para você? Assine o nosso plano mensal Pro Premium VIP e garanta
+          mensalmente um ebook novo de programação. E por menos de um café por
+          dia.{' '}
+        </p>
+        <div>
+          <h3>Plano Pro Premiun VIP</h3>
+        </div>
+        <p>Tudo que você precisa para seus estudos</p>
+        <p>R$29/mês</p>
+
+        <ul>
+          <li>1 ebook por mês</li>
+          <li>Curadoria especial</li>
+          <li>Acesso ilimitado</li>
+          <li>Cancele a qualquer momento</li>
+        </ul>
+        <button>Assina Agora</button>
+      </section>
+
+      <section className="bg-white">
+        <h2>Pronto Para Mudar Sua Vida?</h2>
+        <p>
+          Faça como milhares de outras pessoas. Assine nosso produto e tenha
+          garantido seus estudos{' '}
+        </p>
+        <button>Assine Agora</button>
+        <p>Comece sua assinatura agora mesmo. Cancele quando quiser. </p>
+        <footer>
+          <Image
+            src={logo}
+            alt="Logo"
+            width={16}
+            height={16}
+            style={{ width: 'auto', height: 'auto' }}
+          />
+          <p>© 2024 LivroSaaS. Todos os direitos reservados.</p>
+        </footer>
+      </section>
+    </main>
+  )
+}
